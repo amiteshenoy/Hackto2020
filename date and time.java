671f4e54bfac8bb1954ea4a8aff60b0b032b9ccc
anushkadixit1708 <60402341+anushkadixit1708@users.noreply.github.com>
2020-10-10 01:17:31 +0530

    Program to find current date and time

---
+++ date and time.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.text.*;
+ 
+class DateTimeExample {
+   public static void main(String...s) {
+       DateFormat dateFormate = new SimpleDateFormat("dd-mm-yy hh:mm:ss");
+       Date date = new Date();
+       System.out.println(dateFormate.format(date));
+ 
+       Calendar cal = Calendar.getInstance();
+       System.out.println(dateFormate.format(cal.getTime()));
+    }
+}
